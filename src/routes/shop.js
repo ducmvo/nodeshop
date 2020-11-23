2548031c4087324e9558801af5a6ab8b31ebc30e
Duc Vo <pianote@gmail.com>
2020-11-23 09:08:33 -0800

    refactoring, typscipt config

--- src/routes/shop.js
+++ src/routes/shop.js
@@ -1,8 +1,8 @@
 const path = require('path');
 const express = require('express');
-const productControllers = require('../controllers/products');
+const productController = require('../controllers/products');
 const router = express.Router();
 
-router.get('/', productControllers.getProducts);
+router.get('/', productController.getProducts);
 
 module.exports = router;
