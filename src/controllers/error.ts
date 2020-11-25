096d94a733bc505b07750675431a9930135a90aa
Duc Vo <pianote@gmail.com>
2020-11-24 16:21:28 -0800

    fix styling

--- src/controllers/error.ts
+++ src/controllers/error.ts
@@ -1,9 +1,9 @@
 import { Request, Response, RequestHandler } from 'express';
 
 export const get404: RequestHandler = (req, res) => {
-	res.render('404', {pageTitle: "Not Found", path:''});
+	res.render('404', { pageTitle: 'Not Found', path: '' });
 };
 
-export const getPageNotFound = (req:Request, res:Response): void => {
-	res.render('404', {pageTitle: "Not Found", path:''});
+export const getPageNotFound = (req: Request, res: Response): void => {
+	res.render('404', { pageTitle: 'Not Found', path: '' });
 };
